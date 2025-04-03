0c63a75bf4b3a3b977f2b579db6e791c59006053
Colin Jamison <cjamison@valmagus.com>
2025-04-03 12:41:44 -0700

    Remove more unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import { createSignal } from 'solid-js'
 // import './App.css'
 import {Navbar} from './Navbar.tsx';
 
 function App() {
-  const [count, setCount] = createSignal(0)
 
   return (
-      <Navbar active="index"/>
+      <Navbar />
   )
 }
 
