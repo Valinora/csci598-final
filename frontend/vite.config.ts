f93752deddc59cab04e73bd879593dc45b496d65
Colin Jamison <cjamison@valmagus.com>
2025-04-09 11:22:16 -0700

    Add multiple entry points

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import path from 'node:path'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [solid()],
   resolve: {
     alias: {
-      "~bootstrap": path.resolve(__dirname, 'node_modules/bootstrap')
+      "~bootstrap": resolve(__dirname, 'node_modules/bootstrap')
     }
+  },
+  build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html')
+            },
+            output: {
+                entryFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
   },
   server: {
     host: '0.0.0.0',
