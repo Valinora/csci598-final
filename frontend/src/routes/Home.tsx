e73e31700fea53f433cfae43f34fa4eb7debde9f
Colin Jamison <cjamison@valmagus.com>
2025-04-16 12:00:40 -0700

    Create an API testing page, instead of doing it on Home

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,8 +1,5 @@
 // import './App.css'
 import { Navbar } from '../components/Navbar.tsx';
-import { LoginSignup } from "../components/LoginSignup.tsx";
-import { CreateBathroom } from "../components/CreateBathroom.tsx";
-
 
 export default function App() {
     return (
@@ -10,8 +7,6 @@ export default function App() {
             <Navbar />
             <div>
                 <h1>Bathroom Review App</h1>
-                <LoginSignup />
-                <CreateBathroom />
             </div>
         </>
     );
