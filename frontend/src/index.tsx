a6866e7e85bf3fe63476864c7a55fc5ac71ddcd3
Colin Jamison <cjamison@valmagus.com>
2025-04-16 11:06:15 -0700

    Properly revert index.tsx, and delete files that should have stayed deleted

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,9 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Navbar } from "./Navbar.tsx";
+import { App } from "./App.tsx";
 import "./index.css";
-
-function Home() {
-	return <Navbar active="Home" />;
-}
 
 const root = document.getElementById("root");
 
 // biome-ignore lint/style/noNonNullAssertion: Root must exist, no valid workaround.
-render(() => <Home />, root!);
+render(() => <App />, root!);
