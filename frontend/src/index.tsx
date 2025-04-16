c26d73f93abe280513accc3f4b436b8699f3798a
Colin Jamison <cjamison@valmagus.com>
2025-04-16 12:01:21 -0700

    Dedup the pagemap, and use routes for that.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,16 +6,26 @@ import "./index.css";
 
 const root = document.getElementById("root");
 
-const routes = [
+export const routesLiteral = [
 	{
 		path: "/",
+		name: "Home",
 		component: lazy(() => import("./routes/Home.tsx"))
 	},
 	{
 		path: "/about",
+		name: "About",
 		component: lazy(() => import("./routes/About.tsx"))
+	},
+	{
+		path: "/apitest",
+		name: "API Test",
+		component: lazy(() => import("./routes/Testing.tsx"))
 	}
-]
+] as const;
+
+// Some shuffling to get around `as const`.
+export const routes = [...routesLiteral];
 
 // biome-ignore lint/style/noNonNullAssertion: Root must exist, no valid workaround.
 render(() => <Router>{routes}</Router>, root!);
