42703f99599e7d5d69826b2f8d3801241c8c3952
Valinora <cjamison@valmagus.com>
2025-04-24 10:06:54 -0700

    Start work on displaying nearby bathrooms

--- reststop_rater/views/home.py
+++ reststop_rater/views/home.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
+from ..models.bathroom import Bathroom
+
 
 class HomePage(View):
     template = "index.html"
 
     def get(self, request):
-        return render(request, self.template)
+        bathrooms = Bathroom.get_nearby_bathrooms(5, 5) # TODO: Figure out how to get this from the user and then update the results
+        return render(request, self.template, {"bathrooms": bathrooms})
