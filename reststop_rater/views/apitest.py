92da374476a04193fe9fa627c5ac41a1cac68648
Valinora <cjamison@valmagus.com>
2025-04-24 09:43:58 -0700

    Make login required for API test

--- reststop_rater/views/apitest.py
+++ reststop_rater/views/apitest.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 from ..forms import CreateBathroomForm, ReviewForm
 
 
-class ApiTest(View):
+class ApiTest(LoginRequiredMixin, View):
+    login_url = "/login/"
     template = "apitest.html"
     bathroom_form = CreateBathroomForm
     review_form = ReviewForm
