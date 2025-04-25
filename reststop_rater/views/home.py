4d09127cc997051cd8419d5952dd7873729bdb57
Colin Jamison <cjamison@valmagus.com>
2025-04-25 15:31:58 -0700

    more tempt work

--- reststop_rater/views/home.py
+++ reststop_rater/views/home.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 
-from ..services.bathroom import get_nearby_bathrooms
-
 
 class HomePage(View):
     template = "home.html"
 
     def get(self, request):
-        bathrooms = get_nearby_bathrooms(5, 5) # TODO: Figure out how to get this from the user and then update the results
-        return render(request, self.template, {"bathrooms": bathrooms})
+        return render(request, self.template)
--- reststop_rater/views/nearby.py
+++ reststop_rater/views/nearby.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 
+from ..services.bathroom import get_nearby_bathrooms
+
 class NearbyBathrooms(View):
     template = "nearby.html"
 
 
     def get(self, request):
-        return render(request, self.template)
+        try:
+            lat = float(request.GET["lat"])
+            long = float(request.GET["long"])
+            bathrooms = get_nearby_bathrooms(lat, long)
+
+        except ValueError:
+            print("blerg")
+        bathrooms = get_nearby_bathrooms(5, 5) # TODO: Get this information from the get request.
+        return render(request, self.template, {"bathrooms": bathrooms})
