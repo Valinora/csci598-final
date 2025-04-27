4959a92cad31a6df4f41ef4e01733074521c91e2
Halin Gailey <hgailey@csuchico.edu>
2025-04-27 02:45:01 -0700

    updated reviewform to be compatible with current review submissions

--- reststop_rater/forms.py
+++ reststop_rater/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from .models.bathroom import Bathroom
 from .models.review import Review
 
-
 class JoinForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.EmailField(widget=forms.EmailInput())
@@ -13,19 +12,20 @@ class JoinForm(forms.ModelForm):
         model = User
         fields = ("username", "email", "password")
 
-
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
-
 
 class CreateBathroomForm(forms.ModelForm):
     class Meta:
         model = Bathroom
         exclude = ("rating",)
 
-
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        exclude = ("user","bathroom",)
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'comment': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
