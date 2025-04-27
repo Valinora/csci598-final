d9711a29c9d0a9c2855107a15b10a3cad8370e85
Halin Gailey <hgailey@csuchico.edu>
2025-04-27 02:44:25 -0700

    cleaned up urls and added '/' to nearby

--- final/urls.py
+++ final/urls.py
@@ -9,22 +9,19 @@ from reststop_rater.views.login import UserJoin, UserLogin, user_logout
 from reststop_rater.views.bathrooms import BathroomDetail
 from reststop_rater.views.nearby import NearbyBathrooms
 
-
 def cors_test_view(request):
     return JsonResponse({"message": "CORS works!"})
 
 def home(request):
     return render(request, "home.html")
 
-
 urlpatterns = [
     path("api/cors-test/", cors_test_view),
     path("admin/", admin.site.urls),
-    path("api/", include("reststop_rater.urls")),
-    path("login/", UserLogin.as_view()),
+    path("login/", UserLogin.as_view(), name='login'),
     path("logout/", user_logout),
-    path("join/", UserJoin.as_view()),
+    path("join/", UserJoin.as_view(), name='join'),
     path("", home),
-    path("bathrooms/<int:id>/", BathroomDetail.as_view()),
-    path("nearby", NearbyBathrooms.as_view()),
+    path("bathrooms/<int:id>/", BathroomDetail.as_view(), name='bathroom_detail'),
+    path("nearby/", NearbyBathrooms.as_view()),
 ]
