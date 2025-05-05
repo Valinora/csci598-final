f4c75ca88020151753461eb9524af8105bba3591
Halin Gailey <hgailey@csuchico.edu>
2025-05-05 02:39:35 -0700

    lots of style work. user cna now specify radius.

--- staticfiles/maps.js
+++ staticfiles/maps.js
@@ -3,11 +3,9 @@ function findLocation() {
     const button = document.getElementById("location-btn");
     const spinner = document.getElementById("spinner");
 
-    // Check if the browser supports geolocation
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                // Success: Enable the button and update its text
                 const latitude = position.coords.latitude;
                 const longitude = position.coords.longitude;
 
@@ -15,13 +13,11 @@ function findLocation() {
                 spinner.style.display = "none";
                 button.innerText = "Find Nearby Bathrooms";
 
-                // Attach click event to redirect to /nearby with lat and long as query parameters
                 button.addEventListener("click", () => {
                     window.location.href = `/nearby?lat=${encodeURIComponent(latitude)}&long=${encodeURIComponent(longitude)}`;
                 });
             },
             (error) => {
-                // Error: Display error message
                 console.log(error);
                 button.disabled = false;
                 spinner.style.display = "none";
@@ -33,7 +29,6 @@ function findLocation() {
             }
         );
     } else {
-        // Geolocation not supported
         alert("Geolocation is not supported by your browser.");
         button.disabled = false;
         spinner.style.display = "none";
